30959b80a71e55a22ee9bfca92a692b62d99e0bf
Brett Kaplan <BrettCKaplan@gmail.com>
2024-11-29 23:13:55 -0500

    speed up

--- src/sprite/player.py
+++ src/sprite/player.py
@@ -14,12 +14,12 @@ class Player(jovialengine.GameSprite):
         dx = 0
         dy = 0
         if self._input_frame.get_input_state(0, constants.EVENT_LEFT) == 1:
-            dx -= 32 * 0.001 * dt
+            dx -= 4 * 32 * 0.001 * dt
         if self._input_frame.get_input_state(0, constants.EVENT_RIGHT) == 1:
-            dx += 32 * 0.001 * dt
+            dx += 4 * 32 * 0.001 * dt
         if self._input_frame.get_input_state(0, constants.EVENT_UP) == 1:
-            dy -= 32 * 0.001 * dt
+            dy -= 4 * 32 * 0.001 * dt
         if self._input_frame.get_input_state(0, constants.EVENT_DOWN) == 1:
-            dy += 32 * 0.001 * dt
+            dy += 4 * 32 * 0.001 * dt
         self.pos_x += dx
         self.pos_y += dy
