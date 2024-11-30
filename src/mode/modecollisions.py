9e86ca295e6447fdf7a42651e3071f9db140a370
Brett Kaplan <BrettCKaplan@gmail.com>
2024-11-30 15:53:32 -0500

    Testing movement (#4)
    * ModeBigger
    
    * ModeCollisions
    
    * player object
    
    * moving self
    
    * player sprite
    
    * load image
    
    * speed up
    
    * faster

--- src/mode/modecollisions.py
+++ src/mode/modecollisions.py
@@ -1,6 +1,9 @@
 from .modebigger import ModeBigger
+from sprite import Player
 
 
 class ModeCollisions(ModeBigger):
     def __init__(self):
         super().__init__()
+        player = Player((64, 64))
+        player.start(self)
---
+++ src/sprite/player.py
@@ -0,0 +1,25 @@
+import jovialengine
+
+import constants
+
+
+class Player(jovialengine.GameSprite):
+    _IMAGE_LOCATION = constants.SPRITE_PLAYER
+    _ALPHA_OR_COLORKEY = constants.COLORKEY
+    _COLLISION_MASK_LOCATION = constants.SPRITE_PLAYER
+    _COLLISION_MASK_ALPHA_OR_COLORKEY = constants.COLORKEY
+    _GETS_INPUT = True
+
+    def update(self, dt: int):
+        dx = 0
+        dy = 0
+        if self._input_frame.get_input_state(0, constants.EVENT_LEFT) == 1:
+            dx -= 8 * 32 * 0.001 * dt
+        if self._input_frame.get_input_state(0, constants.EVENT_RIGHT) == 1:
+            dx += 8 * 32 * 0.001 * dt
+        if self._input_frame.get_input_state(0, constants.EVENT_UP) == 1:
+            dy -= 8 * 32 * 0.001 * dt
+        if self._input_frame.get_input_state(0, constants.EVENT_DOWN) == 1:
+            dy += 8 * 32 * 0.001 * dt
+        self.pos_x += dx
+        self.pos_y += dy
