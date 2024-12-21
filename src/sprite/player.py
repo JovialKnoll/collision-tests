110d7bc1971bdfacf8f46f374d7207d68f7d2541
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-20 20:04:29 -0500

    save

--- src/sprite/player.py
+++ src/sprite/player.py
@@ -16,9 +16,10 @@ class Player(jovialengine.GameSprite):
         if self._input_frame.get_input_state(0, constants.EVENT_LEFT) == 1:
             dx -= 8 * 32 * 0.001 * dt
         if self._input_frame.get_input_state(0, constants.EVENT_RIGHT) == 1:
+            print(self.rect)
             dx += 8 * 32 * 0.001 * dt
         if self._input_frame.get_input_state(0, constants.EVENT_UP) == 1:
             dy -= 8 * 32 * 0.001 * dt
         if self._input_frame.get_input_state(0, constants.EVENT_DOWN) == 1:
             dy += 8 * 32 * 0.001 * dt
-        self.pos += (dx, dy)
+        self.rect.move_ip(dx, dy)
