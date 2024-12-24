a868d6de6306f3846a7951fb213e6aa9cb2df783
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-24 13:34:32 -0500

    changes for engine update

--- src/mode/modecollisions.py
+++ src/mode/modecollisions.py
@@ -7,5 +7,5 @@ class ModeCollisions(ModeBigger):
     def __init__(self):
         super().__init__()
         self._background.fill(constants.WHITE)
-        player = Player((64, 64))
+        player = Player(center=(64, 64))
         player.start(self)
