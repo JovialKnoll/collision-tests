d9b8911b0e17cc40c25e344a167f3600e2435828
Brett Kaplan <BrettCKaplan@gmail.com>
2026-02-12 20:47:36 -0500

    switch between modes

--- src/mode/modecircles.py
+++ src/mode/modecircles.py
@@ -1,4 +1,5 @@
 import jovialengine
+import pygame
 
 import constants
 
@@ -7,3 +8,8 @@ class ModeCircles(jovialengine.ModeBase):
     def __init__(self):
         super().__init__()
         self._background.fill(constants.WHITE)
+
+    def _take_event(self, event):
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
+            from .modecollisions import ModeCollisions
+            self.next_mode = ModeCollisions()
