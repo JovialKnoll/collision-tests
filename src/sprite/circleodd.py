6cffd443a0343fe0f1d53317729985683ed14fe4
Brett Kaplan <BrettCKaplan@gmail.com>
2026-02-12 21:06:50 -0500

    not needed

--- src/sprite/circleodd.py
+++ src/sprite/circleodd.py
@@ -11,19 +11,6 @@ class CircleOdd(jovialengine.GameSprite):
     _COLLISION_MASK_ALPHA_OR_COLORKEY = constants.COLORKEY
     _GETS_INPUT = True
 
-    def update(self, dt, camera):
-        dx = 0
-        dy = 0
-        if self._input_frame.get_input_state(0, constants.EVENT_LEFT) == 1:
-            dx -= 8 * 32 * 0.001 * dt
-        if self._input_frame.get_input_state(0, constants.EVENT_RIGHT) == 1:
-            dx += 8 * 32 * 0.001 * dt
-        if self._input_frame.get_input_state(0, constants.EVENT_UP) == 1:
-            dy -= 8 * 32 * 0.001 * dt
-        if self._input_frame.get_input_state(0, constants.EVENT_DOWN) == 1:
-            dy += 8 * 32 * 0.001 * dt
-        self.rect.move_ip(dx, dy)
-
     def _take_state_change(self, state_change: StateChange):
         # pixel-precise movement
         if state_change.new_value == 1:
