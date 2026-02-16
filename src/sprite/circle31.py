c0ac6bcd1603b24a77f317e5afee32be2b1ddde6
Brett Kaplan <BrettCKaplan@gmail.com>
2026-02-15 22:27:23 -0500

    using new sprite

--- src/sprite/circle31.py
+++ src/sprite/circle31.py
@@ -1,9 +1,11 @@
 import pygame
 
+import constants
 from .circle32 import Circle32Even, Circle32Odd
 
 
 class Circle31Even(Circle32Even):
+    _IMAGE_LOCATION = constants.SPRITE_31
     _COLLISION_RADIUS = 15
     _COLOR = (255, 0, 0)
     _COLOR_COLLIDE = (0, 255, 0)
@@ -14,6 +16,7 @@ class Circle31Even(Circle32Even):
 
 
 class Circle31Odd(Circle32Odd):
+    _IMAGE_LOCATION = constants.SPRITE_31
     _COLLISION_RADIUS = 14.5
     _COLOR = (155, 0, 0)
     _COLOR_COLLIDE = (0, 155, 0)
