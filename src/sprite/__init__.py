82f620599cfc2eb48ecd39b7530fe91818cfb341
Brett Kaplan <BrettCKaplan@gmail.com>
2026-02-15 22:38:59 -0500

    use for more tests

--- src/sprite/__init__.py
+++ src/sprite/__init__.py
@@ -1,4 +1,5 @@
 from .circlebase import CircleCollide
 from .circle32 import Circle32Even, Circle32Odd
+from .circle31 import Circle31Even, Circle31Odd
 from .player import Player
 from .star import Star
