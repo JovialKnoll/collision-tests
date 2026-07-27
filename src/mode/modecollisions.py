110753fe27aecb2f208f00b038ab4dfeb5cd4d75
Brett Kaplan <BrettCKaplan@gmail.com>
2026-07-26 22:22:24 -0400

    Test static background collisions (#13)
    * use latest engine version
    
    * add background
    
    * setup
    
    * print logs to show collision

--- src/mode/modecollisions.py
+++ src/mode/modecollisions.py
@@ -1,3 +1,4 @@
+import jovialengine
 import pygame
 
 import constants
@@ -6,13 +7,21 @@ from sprite import Player
 
 
 class ModeCollisions(ModeBigger):
+    _STATIC_COLLISION_MASK_INFOS = (
+        ("test", constants.BACKGROUND_COLLISIONS, constants.COLORKEY),
+    )
+
     def __init__(self):
         super().__init__()
         self._background.fill(constants.WHITE)
+        for info in self._STATIC_COLLISION_MASK_INFOS:
+            image = jovialengine.load.image(info[1], info[2])
+            self._background.blit(image)
         player = Player(center=(64, 64))
         player.start(self)
 
     def _take_event(self, event):
+        super()._take_event(event)
         if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
             from .modecircles import ModeCircles
             self.next_mode = ModeCircles()
